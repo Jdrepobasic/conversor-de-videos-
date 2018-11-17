ced4eefa8d5a146227f37cfcf0bed3222d7a9ba2
Jonathan Dean de Melo Damiani <Jonathan>
2018-11-17 14:40:10 -0200

    teste heroku

---
+++ client/src/components/config.js
@@ -0,0 +1,11 @@
+var config = {}
+
+
+config.zencoder = '7a8f260fcaa329c4475b84c3156a50ad';
+config.outputDb = 's3://convertervideosdb/videos/';
+config.baseURL = 's3://convertervideosdb/';
+config.s3Bucket = 'convertervideosdb';
+config.key = 'AKIAIDA5ZIDZXXJXBZHA';
+config.secretKey = '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';
+
+module.exports = config;
