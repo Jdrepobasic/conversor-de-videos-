445e94e9333ca8a5c92a2363143251e30486f854
Jonathan Dean de Melo Damiani <Jonathan>
2018-11-18 02:28:32 -0200

    ignore

---
+++ client/src/config.js
@@ -0,0 +1,9 @@
+var config = {}
+
+
+config.zencoder = '7a8f260fcaa329c4475b84c3156a50ad';
+config.outputDb = 's3://videosconverterdb/videos/';
+config.baseURL = 's3://videosconverterdb/';
+config.s3Bucket = 'videosconverterdb';
+
+module.exports = config;
